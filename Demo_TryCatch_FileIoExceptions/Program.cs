19e84a7136a00c28bf87cf5fe4b4af1f5e3f5a4d
BuildTools <unconfigured@null.spigotmc.org>
2020-04-19 05:02:44 -0400

    Add project files.

---
+++ Demo_TryCatch_FileIoExceptions/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_TryCatch_FileIoExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileIoStatusMessage;
+            string data;
+
+            data = ReadData(out fileIoStatusMessage);
+
+            Console.WriteLine();
+            Console.WriteLine("File I/O status: {0}", fileIoStatusMessage);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            
+        }
+
+        static string ReadData(out string fileIOStatusMessage)
+        {
+            string dataPath = @"Data/Data.txt";
+            string data = "";
+
+            try
+            {
+                data = File.ReadAllText(dataPath);
+                fileIOStatusMessage = "Complete";
+            }
+            catch (DirectoryNotFoundException)
+            {
+                fileIOStatusMessage = "Unable to locate the folder for the data file.";
+            }
+            catch(FileNotFoundException)
+            {
+                fileIOStatusMessage = "Unable to locate the data file.";
+            }
+            catch(Exception)
+            {
+                fileIOStatusMessage = "Unable to read data file.";
+            }
+
+            return data;
+        }
+    }
+}
